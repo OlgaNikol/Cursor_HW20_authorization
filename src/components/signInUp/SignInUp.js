e7c62e92de90cb7b4af5aa9d5fc058ce4255ce37
Olga <o.palamarenko@gmail.com>
2023-09-05 13:44:39 +0300

    added validation messages, added psw visability

--- src/components/signInUp/SignInUp.js
+++ src/components/signInUp/SignInUp.js
@@ -1,6 +1,8 @@
 import './SignInUp.css';
-import logo from '../../assets/icons/padlock_512.png'
-import copyright from '../../assets/icons/copyright.png'
+import logo from '../../assets/icons/padlock_512.png';
+import copyright from '../../assets/icons/copyright.png';
+import visible from '../../assets/icons/visible.png';
+import hide from '../../assets/icons/hide.png';
 
 import {useState, useEffect} from "react";
 import {Link, useParams} from "react-router-dom";
@@ -13,6 +15,7 @@ export const SignInUp = () => {
     const [stateEmail, setEmail] = useState('');
     const [statePassword, setPassword] = useState('');
     const [stateCheckbox, setCheckbox] = useState(false);
+    const [statePswVisibility, setPswVisibility] = useState(false);
 
     useEffect(() => {
         if (type === 'up') {
@@ -91,22 +94,35 @@ export const SignInUp = () => {
         }
     }
 
+    const togglePswVisibility = () => {
+        const x = document.getElementById("password");
+        if (x.type === "password") {
+            x.type = "text";
+            setPswVisibility(true);
+        } else {
+            x.type = "password";
+            setPswVisibility(false);
+        }
+    }
+
     return (
         <div className="container">
             <img className="logo" src={logo} alt="logo"/>
             <h1>Sign {type === 'in' ? 'in' : 'up'}</h1>
             <form onSubmit={(e) => handleSubmit(e)} className="form">
-                { (type === 'up') &&
+                {(type === 'up') &&
                     <div className="wrapper-inputs">
                         <input value={stateFirstName} type="text"
                                onChange={handleFirstNameChange}
                                placeholder="First Name *"
                                pattern="[a-zA-Zа-яА-Я]{3,}"
+                               title="Must contain at least 3 characters"
                                required/>
                         <input value={stateLastName} type="text"
                                onChange={handleLastNameChange}
                                pattern="[a-zA-Zа-яА-Я]{3,}"
                                placeholder="Last Name *"
+                               title="Must contain at least 3 characters"
                                required/>
                     </div>
                 }
@@ -114,39 +130,54 @@ export const SignInUp = () => {
                 <input type="text" value={stateEmail}
                        onChange={handleEmailChange}
                        placeholder="Email Address *"
-                       required
-                       pattern="[a-z0-9_\-]{3,}@[a-z0-9_\-]{2,}\.[a-z]{2,}$"/>
-                <input type="text" value={statePassword}
+                       pattern="[a-z0-9_\-]{3,}@[a-z0-9_\-]{2,}\.[a-z]{2,}$"
+                       title="Must contain: at least 3 characters, @ symbol, at least 2 characters, period and at least 2 characters"
+                       required/>
+                <input type="password" value={statePassword}
+                       id="password"
                        onChange={handlePasswordChange}
                        placeholder="Password *"
                        pattern="(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}"
-                       required/>
+                       title="Must contain at least 8 characters, contains one uppercase and one lowercase Latin letters"
+                       required>
+                </input>
+                <div className="icons">
+                    <img className="eye"
+                         onClick={() => togglePswVisibility()}
+                         style={{'display': statePswVisibility ? 'block' : 'none'}}
+                         src={visible} alt="eyeVisible"/>
+                    <img className="eye"
+                         onClick={() => togglePswVisibility()}
+                         style={{'display': !statePswVisibility ? 'block' : 'none'}}
+                         src={hide} alt="eyeInvisible"/>
+                </div>
 
-                { (type === 'in') &&
+                {(type === 'in') &&
                     <div className="wrapper-checkbox">
-                        <input className="checkbox" checked={stateCheckbox} onChange={handleCheckbox} type="checkbox"
-                               placeholder="Password *"/>
-                        <label>Remember me</label>
+                        <input id="rememberMe" className="checkbox" checked={stateCheckbox} onChange={handleCheckbox}
+                               type="checkbox"/>
+                        <label htmlFor="rememberMe">Remember me</label>
                     </div>
                 }
-                { (type === 'up') &&
+                {(type === 'up') &&
                     <div className="wrapper-checkbox">
-                        <input className="checkbox" checked={stateCheckbox} onChange={handleCheckbox} type="checkbox"
-                               placeholder="Password *"/>
-                        <label className="checkbox-label">I want to receive inspiration, marketing promotions and
+                        <input id="promotions" className="checkbox" checked={stateCheckbox} onChange={handleCheckbox}
+                               type="checkbox"/>
+                        <label htmlFor="promotions" className="checkbox-label">I want to receive inspiration, marketing
+                            promotions and
                             updates via email.</label>
                     </div>
                 }
 
                 <button>Sign {type === 'in' ? 'in' : 'up'}</button>
 
-                { (type === 'in') &&
+                {(type === 'in') &&
                     <div className="wrapper-links">
                         <p>Forgot password?</p>
                         <Link to='/Cursor_HW20_authorization/signInUp/up'>Don't have an account? Sign up</Link>
                     </div>
                 }
-                { (type === 'up') &&
+                {(type === 'up') &&
                     <div className="wrapper-links">
                         <p></p>
                         <Link to='/Cursor_HW20_authorization/signInUp/in'>Already have an account? Sign in</Link>
